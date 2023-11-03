e4700dad5a089408ad3c008c88307061256ba085
UNDEADBRO <LOchoa957@gmail.com>
2023-11-03 14:44:23 -0700

    Separated Favorites Page
    Separated Favorite page into two but can't check if it works since no db to connect to

--- protoype_v2/favorites_db_connector.py
+++ protoype_v2/favorites_db_connector.py
@@ -1,1 +1,16 @@
 #this file will connect the favorites page to the database to retrieve the user's favorites meals
+#Author: Luis O.
+
+
+import sqlite3
+
+# Function to list favorite meals for a user
+def list_favorite_meals(user_id):
+    conn = sqlite3.connect("favorite_meals.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM favorite_meals WHERE user_id=?", (user_id,))
+    favorite_meals = cursor.fetchall()
+
+    conn.close()
+    return(favorite_meals)
--- protoype_v2/main.py
+++ protoype_v2/main.py
@@ -1,5 +1,5 @@
 import account_page as ap
-import favorites_page
+import favorites_page as fp
 import home_page
 import login
 
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     user_id = login.login()
     
     while(user_id != None):
+        fp.list_favorite_meals(user_id)
         ap.account_page(user_id)
     
